c8670fd9bda0f39d8e7cd3d2501ab1988d1d376b
davidiab90 <davidiab90@gmail.com>
2025-10-30 23:58:34 -0600

    Launch Dremars fresh: Flask + templates/static base

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request, jsonify
+import requests
+import base64
+import os
+import traceback
+from dotenv import load_dotenv
+
+load_dotenv()  # Carga las variables de entorno
+
+
+
+app = Flask(__name__)
+
+
+# Rutas
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/project')
+def project():
+    return render_template('project.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/project_original_designs')
+def project_original_designs():
+    return render_template('project_original_designs.html')
+
+@app.route('/project_physical_projects')
+def project_physical_projects():
+    return render_template('project_physical_projects.html')
+
+@app.route('/project_interior_plans')
+def project_interior_plans():
+    return render_template('project_interior_plans.html')
+
+
+@app.route('/ai_rendering')
+def ai_rendering():
+    return render_template('ai_rendering.html')
+
+@app.route('/ai_interior_modeling')
+def ai_interior_modeling():
+    return render_template('ai_interior_modeling.html')
+
+@app.route('/ai_solutions')
+def ai_solutions():
+    return render_template('ai_solutions.html')
+
+
+
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+if __name__ == "__main__":
+    app.run(debug=True)
